66f129c6165f19ad24a65f1a9c34119221236b67
Viorel Breban <vbreban69@gmail.com>
2024-07-23 23:53:35 +0200

    last one

--- app.js
+++ app.js
@@ -1,47 +1,4 @@
-// import express, { json } from "express";
-// import logger from "morgan";
-// import cors from "cors";
-// import swaggerUi from "swagger-ui-express";
-// import JSON5 from "json5";
-// import fs from "fs";
-// import authRouter from "./routes/api/auth.js";
-// import dashboardRouter from "./routes/api/dashboards.js";
-// import columnRouter from "./routes/api/column.js";
-// import cardRouter from "./routes/api/card.js";
-// import "dotenv/config";
-
-// const app = express();
-
-// const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-
-// const swaggerDocument = JSON5.parse(fs.readFileSync("./swagger.json", "utf-8"));
-
-// app.use(logger(formatsLogger));
-// app.use(cors());
-// app.use(json());
-
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// app.use("/api/users", authRouter);
-
-// app.use("/api/dashboard", dashboardRouter);
-
-// app.use("/api/column", columnRouter);
-
-// app.use("/api/card", cardRouter);
-
-// app.use((_req, res) => {
-//   res.status(404).json({ message: "Not found on 3000" });
-// });
-
-// app.use((err, _req, res, _next) => {
-//   const { status = 500, message = "Server error" } = err;
-//   res.status(status).json({ message });
-// });
-
-// export default app;
-
-import express from "express";
+import express, { json } from "express";
 import logger from "morgan";
 import cors from "cors";
 import swaggerUi from "swagger-ui-express";
@@ -54,7 +11,6 @@ import cardRouter from "./routes/api/card.js";
 import "dotenv/config";
 
 const app = express();
-const { PORT = 3000 } = process.env;
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
@@ -62,13 +18,16 @@ const swaggerDocument = JSON5.parse(fs.readFileSync("./swagger.json", "utf-8"));
 
 app.use(logger(formatsLogger));
 app.use(cors());
-app.use(express.json());
+app.use(json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/api/users", authRouter);
+
 app.use("/api/dashboard", dashboardRouter);
+
 app.use("/api/column", columnRouter);
+
 app.use("/api/card", cardRouter);
 
 app.get("/", (req, res) => {
@@ -76,16 +35,12 @@ app.get("/", (req, res) => {
 });
 
 app.use((_req, res) => {
-  res.status(404).json({ message: "Not found on " + PORT });
+  res.status(404).json({ message: "Not found on 3000" });
 });
 
 app.use((err, _req, res, _next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
 });
 
 export default app;
